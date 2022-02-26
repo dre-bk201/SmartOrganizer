702f4330014bd226336d07723964e155b041124c
Aundre Bryan <aundrebryan123@gmail.com>
2022-02-26 17:25:10 -0500

    Create index.ts
    Added actions for future flexibility.

---
+++ src/utils/index.ts
@@ -0,0 +1,10 @@
+export const FILENAME = "File Name";
+export const FILEEXTENSION = "File Extension";
+export const FOLDERNAME = "Folder Name";
+export const FILECONTENT = "File Content";
+export const FILESIZE = "File Size";
+export const MOVE = "MOVE";
+export const COPY = "COPY";
+export const RENAME = "RENAME";
+export const UNLINK = "UNLINK";
+export const DELETE = "DELETE";
