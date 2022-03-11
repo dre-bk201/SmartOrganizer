c5c0b6f168e2346d1f622e48a4b5c2bda421df07
Aundre Bryan <aundrebryan123@gmail.com>
2022-03-11 14:35:06 -0500

    Refactor Log
    Updated Log props to separate a log's id from it's parent's id. Added ability to open valid paths on click for corresponding Log chip.

--- src/components/Log.vue
+++ src/components/Log.vue
@@ -1,25 +1,42 @@
 <script lang="ts" setup>
-import { Listener, Log } from "../store/modules/listener";
-
 import Icon from "../components/Icon.vue";
-import { ref } from "vue";
+
+import { open } from "@tauri-apps/api/shell";
+import { invoke } from "@tauri-apps/api";
+import { computed, ref } from "vue";
+import { useStore } from "vuex";
 
 interface Props {
-  listener: Listener;
-  log: Log;
+  parent_id: string;
+  id: string;
+  idx: number;
+  path: string;
+  action: string;
+  timestamp: string;
+  destination: string;
 }
 
-defineProps<Props>();
+const props = defineProps<Props>();
+const store = useStore();
 
 let detail = ref("");
+let title = store.getters["listener/getById"](props.parent_id)?.title;
 
-const setInfo = (value: string = "") => {
-  detail.value = value;
-};
+const setInfo = (value: string = "") => (detail.value = value);
+
+const unsupported = computed(
+  () => !["DELETE", "UNLINK"].includes(props.action)
+);
 
 const undoAction = () => {
-  // invoke a method to tauri to reverse command
+  invoke("undo_action", {
+    id: props.id,
+    from: props.path,
+    action: [props.action, props.destination],
+  });
 };
+
+const openDir = async (path: string) => await open(path);
 </script>
 
 <template>
@@ -35,67 +52,78 @@ const undoAction = () => {
     <section class="flex w-full px-4 pr-2 mb-1">
       <div class="icon--bg rounded-full p-2 bg-l_primary mr-5">
         <Icon
-          :name="log.action.toLowerCase()"
+          :name="action?.toLowerCase()"
           width="30px"
           height="30px"
           fill="black"
         />
       </div>
       <div class="grow">
-        <header>{{ listener.title }}</header>
-        <span> Last updated: {{ log.timestamp }} </span>
+        <header>{{ title }}</header>
+        <span>
+          Updated At:
+          {{
+            new Date(timestamp).toLocaleString("en-US", {
+              year: "numeric",
+              month: "numeric",
+              day: "numeric",
+              hour: "numeric",
+              minute: "numeric",
+              second: "numeric",
+            })
+          }}
+        </span>
       </div>
       <button
+        v-show="unsupported"
         @click="undoAction"
-        class="bg-[#CBCBCB] dark:bg-d_primary px-1 h-8 rounded-md"
+        class="bg-[#CBCBCB] dark:bg-d_primary px-2 h-8 rounded-md"
       >
         UNDO
       </button>
     </section>
 
     <section class="grid-view md:grid-view-md px-2">
-      <header class="ml-5">Monitor Dir</header>
+      <header class="ml-5">Monitoring</header>
       <header class="ml-2">Action</header>
       <header class="ml-5">Destination</header>
       <header class="ml-2">Timestamp</header>
 
       <!-- Monitor Chip -->
       <span
-        @mouseover="setInfo(log.path)"
+        @mouseover="setInfo(path)"
+        @click="openDir(path)"
         @mouseout="setInfo('')"
         class="log--card max-w-fit text-[#FF5C5C] relative bg-[#FF5C5C]"
-        >{{ log.path }}
+        >{{ path }}
         <div class="dot bg-[#FF5C5C]" />
       </span>
 
       <!-- Action Chip -->
       <span
-        @mouseover="setInfo(log.action)"
+        @mouseover="setInfo(action)"
         @mouseout="setInfo('')"
         class="rounded-full text-center hover:cursor-pointer relative text-[#00F0FF] bg-[#00E9F8] bg-opacity-30 min-w-fit max-w-fit pl-4 px-3"
-        >{{ log.action }}
+        >{{ action }}
         <div class="dot bg-[#00F0FF]" />
       </span>
 
       <!-- Destination Chip -->
       <span
-        @mouseover="setInfo(log.destination)"
+        @mouseover="setInfo(destination)"
+        @click="openDir(destination)"
         @mouseout="setInfo('')"
         class="log--card max-w-fit text-[#4200FE] bg-[#4200FE] dark:bg-[#EBFF6F] dark:text-[#EBFF6F] dark:bg-opacity-30"
-        >{{
-          log.destination
-            ? log.destination
-            : "No Directory Required For Operation"
-        }}
+        >{{ destination ? destination : "No Directory Required For Operation" }}
         <div class="dot bg-[#4200FE] dark:bg-[#EBFF6F]" />
       </span>
 
       <!-- Timestamp Chip -->
       <span
-        @mouseover="setInfo(log.timestamp)"
+        @mouseover="setInfo(timestamp)"
         @mouseout="setInfo('')"
         class="log--card text-[#FF00A8] max-w-fit bg-[#FF00A8]"
-        >{{ log.timestamp }}
+        >{{ timestamp }}
         <div class="dot bg-[#FF00A8]" />
       </span>
     </section>
@@ -106,22 +134,17 @@ const undoAction = () => {
 /* .slide-enter-active {
   transform: translateY(17px);
 }
-
 .slide-leave-active {
 }
-
 .slide-enter-from,
 .slide-leave-to {
 } */
-
 .slide-fade-enter-active {
   transition: all 0.3s ease-out;
 }
-
 .slide-fade-leave-active {
   transition: all 0.3s cubic-bezier(1, 0.5, 0.8, 1);
 }
-
 .slide-fade-enter-from,
 .slide-fade-leave-to {
   transform: translateX(20px);
