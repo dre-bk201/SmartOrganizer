453f3e072b9d764932b4785871162c612884eccd
Aundre Bryan <aundrebryan123@gmail.com>
2022-03-31 16:02:57 -0500

    Update Titlebar.vue

--- src/components/Titlebar.vue
+++ src/components/Titlebar.vue
@@ -1,25 +1,31 @@
 <script lang="ts" setup>
 import Icon from "./Icon.vue";
 
-import { computed, ComputedRef, inject, ref, toRefs } from "vue";
+import { computed, ComputedRef, inject, toRefs } from "vue";
 import { useStore } from "vuex";
 import { Log } from "../store/modules/listener";
 import { useDimensions } from "../utils";
+import { useRoute } from "vue-router";
 
+const route = useRoute();
 const store = useStore();
 const dimensions = useDimensions();
 
-const isDark = inject("isDark");
-const log: ComputedRef<Log> | undefined = inject("logDetail");
+// Variables
+let log: ComputedRef<Log> | undefined = inject("logDetail");
 
 // Refs
 const { isMaximized, appWindow } = toRefs(dimensions);
 
 // Computed
+const isDark = computed(() => store.getters["config/isDark"]);
+
 const isCleaning = computed(() => store.getters.triggerClean);
+
 const osType: ComputedRef<"win32" | "macos"> = computed(
-  () => store.getters["settings/titlebar"]
+  () => store.getters["config/titlebar"]
 );
+
 // Functions
 const close = () => appWindow.value.close();
 
@@ -27,8 +33,7 @@ const minimize = () => appWindow.value.minimize();
 
 const toggleMaximize = () => appWindow.value.toggleMaximize();
 
-const toggleTheme = () =>
-  document.getElementById("app")?.classList.toggle("dark");
+const openSettings = () => store.dispatch("toggleSettings", true);
 </script>
 
 <template>
@@ -38,13 +43,13 @@ const toggleTheme = () =>
     class="min-h-[32px] max-h-[32px] bg-l_secondary dark:bg-d_secondary flex items-center pl-5 relative"
   >
     <span data-tauri-drag-region class="text-gray-800 dark:text-[#b6b6b6]">
-      {{ isCleaning ? "Path: " + log?.path : "" }}
+      {{ isCleaning && route.name != "Journal" ? "Path: " + log?.path : "" }}
     </span>
     <Icon
       class="absolute right-36 hover:cursor-pointer"
+      @click="openSettings"
       :fill="isDark ? '#b6b6b6' : 'black'"
       :key="(isDark as number)"
-      @click="toggleTheme"
       name="settings"
       height="20"
       width="20"
@@ -53,7 +58,7 @@ const toggleTheme = () =>
     <div class="flex h-full hover:cursor-pointer absolute right-0">
       <Icon
         @click="minimize"
-        class="titlebar--icon right-36 px-3 center hover:bg-slate-500"
+        class="titlebar--icon right-36 px-3 center hover:bg-slate-300"
         :key="(isDark as number)"
         name="minimize"
         height="2"
@@ -63,7 +68,7 @@ const toggleTheme = () =>
 
       <Icon
         @click="toggleMaximize"
-        class="titlebar--icon px-3 center hover:bg-slate-500"
+        class="titlebar--icon px-3 center hover:bg-slate-300"
         :key="(isDark as number)"
         :name="isMaximized ? 'restore' : 'maximize'"
         height="15"
@@ -82,12 +87,15 @@ const toggleTheme = () =>
       />
     </div>
   </div>
+
   <div
     v-else-if="osType == 'macos'"
     data-tauri-drag-region
     class="min-h-[32px] max-h-[32px] bg-l_secondary dark:bg-d_secondary flex items-center pl-5"
   >
-    <div class="flex items-center justify-around absolute left-1 w-[80.4px]">
+    <div
+      class="flex items-center justify-around absolute left-[10px] w-[80.4px]"
+    >
       <div
         @click="close"
         class="hover:cursor-pointer bg-[#ff5259] w-[13.8px] h-[13.8px] rounded-full"
@@ -104,9 +112,9 @@ const toggleTheme = () =>
 
     <Icon
       class="absolute left-28 hover:cursor-pointer"
+      @click="openSettings"
       :fill="isDark ? '#b6b6b6' : 'black'"
       :key="(isDark as number)"
-      @click="toggleTheme"
       name="settings"
       height="20"
       width="20"
@@ -116,7 +124,7 @@ const toggleTheme = () =>
       data-tauri-drag-region
       class="text-gray-800 absolute left-36 dark:text-[#b6b6b6]"
     >
-      {{ isCleaning ? "Path: " + log?.path : "" }}
+      {{ isCleaning && route.name != "" ? "Path: " + log?.path : "" }}
     </span>
   </div>
 </template>
