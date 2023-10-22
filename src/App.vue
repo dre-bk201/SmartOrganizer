96b615c8465a7205f24b99a33cc27094c14dc03b
Aundre Bryan <48803238+dre-bk201@users.noreply.github.com>
2023-10-21 23:54:02 -0500

    update: app relaunches on non-windows platforms

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,7 @@ import {
 import { reactive, nextTick, computed, provide, onMounted, ref } from "vue";
 import { invoke } from "@tauri-apps/api/tauri";
 import { listen } from "@tauri-apps/api/event";
+import { platform } from "@tauri-apps/api/os";
 import { FocusOutsideEvent, PointerDownOutsideEvent } from "node_modules/radix-vue/dist/DismissableLayer";
 
 import {
@@ -108,36 +109,22 @@ onMounted(() => {
     if (settingStore.theme === 'dark') document.documentElement.classList.add('dark');
     else document.documentElement.classList.remove('dark');
 
-
+    // UPDATER
     const unlisten = await onUpdaterEvent(({ error, status }) => {
-      // This will log all updater events, including status updates and errors.
       console.log('Updater event', error, status)
     })
 
     try {
-      const { shouldUpdate, manifest } = await checkUpdate()
-
+      const { shouldUpdate } = await checkUpdate()
       if (shouldUpdate) {
-        // You could show a dialog asking the user if they want to install the update here.
-        console.log(
-          `Installing update ${manifest?.version}, ${manifest?.date}, ${manifest?.body}`
-        )
-
-        // Install the update. This will also restart the app on Windows!
         await installUpdate()
-
-        // On macOS and Linux you will need to restart the app manually.
-        // You could use this step to display another confirmation dialog.
-        await relaunch()
+        if(await platform() !== "win32") await relaunch()
       }
     } catch (error) {
       console.error(error)
     }
 
-    // you need to call unlisten if your handler goes out of scope, for example if the component is unmounted.
     unlisten()
-
-
   })();
 
 
